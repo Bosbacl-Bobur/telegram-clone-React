6e81f82de55e90ec89252cf672a06c1234ed9d8f
Bosbacl_Bobur <bosbacl713@gmail.com>
2025-09-21 17:51:52 +0500

    add

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Telegram Clone Backend 🚀");
+});
+
+// MongoDB ulanish
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running on http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => console.error("❌ DB connection error:", err));
