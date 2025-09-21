d647397c28a76b5f4cbb8a5a64d566ecba9302f8
Bosbacl_Bobur <bosbacl713@gmail.com>
2025-09-21 17:38:31 +0500

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1 className=''>Heelo</h1>
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Telegram Clone</h1>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
